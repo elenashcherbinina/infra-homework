28212964ca1d051aa0ebd3dc47006c3c1e60a419
Elena Shcherbinina <el.shcherbinina@outlook.com>
2024-12-15 18:37:04 +0300

    [TASK-1] feat: add loader.cjs and loader.mjs

--- build/loader.cjs
+++ build/loader.cjs
@@ -1,2 +1,16 @@
 const yaml = require('yaml');
 const fs = require('node:fs');
+const path = require('path');
+
+require.extensions['.yml'] = function (module, filename) {
+  const content = require('fs').readFileSync(filename, 'utf8');
+  const parsedContent = yaml.parse(content);
+  module.exports = parsedContent;
+};
+
+const myScript = require(path.resolve(__dirname, '../build/test/config.yml'));
+console.log(myScript);
+
+module.exports = {
+  myScript,
+};
--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,24 @@
 import yaml from 'yaml';
 import path from 'node:path';
-import fs from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+
+export async function load(url, context, nextLoad) {
+  if (url.endsWith('.yml') || url.endsWith('.yaml')) {
+    try {
+      const filePath = path.resolve(fileURLToPath(url)); // нормализуем путь
+      const source = readFileSync(filePath, 'utf8'); // читаем файл
+
+      return {
+        format: 'json',
+        shortCircuit: true,
+        source: JSON.stringify(yaml.parse(source)),
+        парсим,
+      };
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return nextLoad(url, context);
+}
